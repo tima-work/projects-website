988b1a9d2c22c26dc201038d904633e0b258ada0
Tymofii Andreiev <tandreievworkacc@gmail.com>
2025-04-22 11:49:53 +0200

    Tried fixing CV page

--- vite-project/src/pages/CVPage.jsx
+++ vite-project/src/pages/CVPage.jsx
@@ -9,23 +9,13 @@ export default function CVPage() {
     const { isDarkMode } = useDarkMode();
 
 
-    const handleDownloadPdf = () => {
-        // Download logic for PDF format
-        const link = document.createElement("a");
-        link.href = "/public/downloads/My CV.pdf";
-        link.download = "My CV.pdf";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
-
     return (
         <Layout>
             <div className="flex flex-col items-center w-full h-full gap-6 py-8">
                 {/* PDF Viewer */}
                 <div className="w-full max-w-5xl h-[1100px]">
                     <iframe
-                        src={`/public/downloads/My CV.pdf`}
+                        src={`/downloads/My CV.pdf`}
                         className="w-full h-full border border-gray-300 rounded"
                         title="CV Preview"
                     />
@@ -33,15 +23,15 @@ export default function CVPage() {
 
                 {/* Download buttons */}
                 <div className="flex flex-col md:flex-row gap-4">
-                    <DownloadButton link={"/public/downloads/My CV.docx"} isDarkMode={isDarkMode}>
+                    <DownloadButton link={"/downloads/My CV.docx"} isDarkMode={isDarkMode}>
                         Download DOCX
                         {<FileText />}
                     </DownloadButton>
-                    <DownloadButton link={"/public/downloads/My CV.pdf"} isDarkMode={isDarkMode}>
+                    <DownloadButton link={"/downloads/My CV.pdf"} isDarkMode={isDarkMode}>
                         Download PDF
                         {<DownloadIcon />}
                     </DownloadButton>
-                    <DownloadButton link={"/public/downloads/My CV.png"} isDarkMode={isDarkMode}>
+                    <DownloadButton link={"/downloads/My CV.png"} isDarkMode={isDarkMode}>
                         Download PNG
                         <ImageDownIcon />
                     </DownloadButton>
